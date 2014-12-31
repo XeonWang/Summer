53cf33c3b5cdd16337abbce9fa9225c4eeeccca1
XeonWang <wangzhiqiang1026@gmail.com>
2014-12-31 19:04:28 +0800

    Clean ClassPathXmlApplicationContext to prepare reimplement.

--- src/org/summer/bean/ClassPathXmlApplicationContext.java
+++ src/org/summer/bean/ClassPathXmlApplicationContext.java
@@ -31,104 +31,14 @@ public class ClassPathXmlApplicationContext extends ApplicationContext {
 		InputStream fileInput = this.getClass().getClassLoader().getResourceAsStream(configurationFileName);
 		try {
 			NodeList beanNodes = extractBeanNodes(fileInput);
-			for (int i = 0; i < beanNodes.getLength(); i++) {
-				Node bean = beanNodes.item(i);
-				createBean(bean);
-			}
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
 
-	private void createBean(Node bean) {
-		if(bean.getNodeName().equals("bean")) {
-			try {
-				String beanName = XmlUtils.getNamedAttribute(bean, "id");
-				String beanClazz = XmlUtils.getNamedAttribute(bean, "class");
-				
-				Map<String, PropertyValue> properties = extractProperties(bean);
-				
-				Class<?> desiredClass = Class.forName(beanClazz);
-				Constructor<?> constructor = desiredClass.getConstructor(new Class[0]);
-				Object obj = constructor.newInstance(new Object[0]);
-				
-				invokeSetters(properties, desiredClass, obj);
-				
-				beans.put(beanName, obj);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
-	private void invokeSetters(Map<String, PropertyValue> properties,
-			Class<?> desiredClass, Object obj) throws NoSuchMethodException,
-			IllegalAccessException, InvocationTargetException {
-		
-		Method[] methods = desiredClass.getMethods();
-		
-		for (String key : properties.keySet()) {
-			PropertyValue value = properties.get(key);
-			String setterName = getSetterName(key);
-			
-			for (Method method : methods) {
-				if (method.getName().equals(setterName)) {
-					Type[] types = method.getGenericParameterTypes();
-					if (value.isSuitableForType(types[0])) {
-						method.invoke(obj, new Object[]{value.rightType(types[0])});
-					}
-				
-//					Object typedValue = covertToRightType(value, types[0]);
-//					
-//					if(typedValue != null) {
-//						method.invoke(obj, new Object[]{typedValue});
-//						break;
-//					}
-				}
-			}
-			
-		}
-	}
-
-//	private Object covertToRightType(PropertyValue value, Type type) {
-////		if(((Class<?>)type).isInstance(value)) return value;
-//		Converter converter = ConvertFactory.getConverter(type);
-//		if (converter != null) return converter.getValue((String)value.);
-//		return null;
-//	}
-
-	private Map<String, PropertyValue> extractProperties(Node bean) {
-		Map<String, PropertyValue> properties = new HashMap<String, PropertyValue>();
-		NodeList subNodes = bean.getChildNodes();
-		for (int i = 0; i < subNodes.getLength(); i++) {
-			Node sub = subNodes.item(i);
-			if(sub.getNodeName().equals("property")) {
-				properties.put(XmlUtils.getNamedAttribute(sub, "name"), getPropertyValue(sub));
-			}
-		}
-		return properties;
-	}
-	
-	private PropertyValue getPropertyValue(Node node) {
-		String valueAttr = XmlUtils.getNamedAttribute(node, "value");
-		if (valueAttr != null) return new StringPropertyValue(valueAttr);
-
-		NodeList contentNodes = node.getChildNodes();
-		for (int i = 0; i < contentNodes.getLength(); i++) {
-			Node subOfProperty = contentNodes.item(i);
-			if("ref".equalsIgnoreCase(subOfProperty.getNodeName())) {
-				return new BeanPropertyValue(XmlUtils.getNamedAttribute(subOfProperty, "bean"));
-			}
-		}
-		
-		return new StringPropertyValue(node.getTextContent());
-	}
-
-	private String getSetterName(String key) {
-		String capitalizedKey = StringUtils.capitalize(key);
-		return "set" + capitalizedKey;
-	}
 
 	private NodeList extractBeanNodes(InputStream fileInput)
 			throws ParserConfigurationException, SAXException, IOException {
